e79cdcd9c7c7e0b8c22bfee5afdecab64cbfa672
EvgeneOskin <eoskin@crystalnix.com>
2018-05-05 21:23:29 +1200

    Add simplest JSON based REST API with serde

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,39 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
-#[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-  format!("Hello, {} year old named {}!", age, name)
+use rocket_contrib::{Json, Value};
+
+mod hero;
+use hero::{Hero};
+
+#[post("/hero", data="<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
+  hero
 }
 
+#[get("/hero")]
+fn read() -> Json<Value> {
+  Json(json!([
+    "hero 1",
+    "hero 2",
+  ]))
+}
+
+#[put("/hero/<id>", data="<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
+  hero
+}
+
+#[delete("/hero/<id>")]
+fn delete(id: i32) -> Json<Value> {
+  Json(json!({"status": "ok"}))
+}
 
 fn main() {
     rocket::ignite()
-    .mount("/hello", routes![hello])
+    .mount("/", routes![create, read, update, delete])
     .launch();
 }
